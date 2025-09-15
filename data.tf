3ca28719543661d86ffdb95bb366daa6d7b9b552
ManashRoyINT <manas.roy@indusnet.co.in>
2025-09-15 15:14:34 +0530

    updated the logic of fetching the default security_group

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_security_group" "default" {
-  count = var.security_group_ids == [] ? 1 : 0
+  count = var.security_group_ids == [] && !var.create_security_group ? 1 : 0
 
   name   = "default"
   vpc_id = local.vpc_id
